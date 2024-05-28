dac6d95e9e0fedb929e0562d8be4018868c1a542
Adam Rodgers <arodgers@gmail.com>
2024-05-28 10:44:30 -0500

    More Vercel-friendly

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,12 @@
-// next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    NEXTAUTH_SECRET: "8tB65hyR3rpy5gxWI+Q9F/S+noG6d4NkB5AGf2PMbrM=",
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL || "http://localhost:3000",
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET || "",
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID || "",
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET || "",
+    MONGODB_URI: process.env.MONGODB_URI || "",
+    MONGODB_DB: process.env.MONGODB_DB || "",
   },
 };
 
