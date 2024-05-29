993b59b45cba56664f5de1c578470dcde94ad3a2
Adam Rodgers <arodgers@gmail.com>
2024-05-29 12:27:34 -0500

    Split database data per userId

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import { connectToDatabase } from "../../../utils/mongodb";
+import { connectToDatabase } from "../../../../utils/mongodb";
 
 const authOptions = {
   providers: [
