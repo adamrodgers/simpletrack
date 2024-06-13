103db1b39da9042e328b6d6130fe7f15b020d38e
Adam Rodgers <arodgers@gmail.com>
2024-06-13 10:41:15 -0500

    Insurance Needs wrap to new line when more than 3 exist

--- src/components/InsuranceNeeds.js
+++ src/components/InsuranceNeeds.js
@@ -3,21 +3,42 @@ import PropTypes from "prop-types";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { getNeedLevel } from "../utils/insurableItems";
 
-const InsuranceNeeds = ({ needs }) => (
-  <td className="px-6 py-4">
-    <div className="flex gap-2">
-      {needs.sort().map((need, idx) => {
-        const { bgColor, textColor, icon } = getNeedLevel(need);
-        return (
-          <span key={idx} className={`inline-flex items-center gap-1 rounded-full ${bgColor} px-2 py-1 text-xs font-semibold ${textColor}`}>
-            <FontAwesomeIcon icon={icon} className="mr-1" />
-            {need}
-          </span>
-        );
-      })}
-    </div>
-  </td>
-);
+const InsuranceNeeds = ({ needs }) => {
+  const firstRow = needs.slice(0, 3);
+  const secondRow = needs.slice(3);
+
+  return (
+    <td className="px-6 py-4">
+      <div className="flex flex-wrap gap-1">
+        {" "}
+        {/* Adjusted gap for less spacing */}
+        {firstRow.map((need, idx) => {
+          const { bgColor, textColor, icon } = getNeedLevel(need);
+          return (
+            <span key={idx} className={`inline-flex items-center gap-1 rounded-full ${bgColor} px-2 py-1 text-xs font-semibold ${textColor}`}>
+              <FontAwesomeIcon icon={icon} className="mr-1" />
+              {need}
+            </span>
+          );
+        })}
+        {secondRow.length > 0 && (
+          <>
+            <span className="w-full h-px"></span> {/* This forces the next items to a new line */}
+            {secondRow.map((need, idx) => {
+              const { bgColor, textColor, icon } = getNeedLevel(need);
+              return (
+                <span key={idx} className={`inline-flex items-center gap-1 rounded-full ${bgColor} px-2 py-1 text-xs font-semibold ${textColor}`}>
+                  <FontAwesomeIcon icon={icon} className="mr-1" />
+                  {need}
+                </span>
+              );
+            })}
+          </>
+        )}
+      </div>
+    </td>
+  );
+};
 
 InsuranceNeeds.propTypes = {
   needs: PropTypes.arrayOf(PropTypes.string).isRequired,
