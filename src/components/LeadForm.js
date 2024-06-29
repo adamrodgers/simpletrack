d41579e4d0e7fcc24f29a303d6a1d53f8194a45d
Adam Rodgers <arodgers@gmail.com>
2024-06-29 11:26:19 -0500

    Small fix

--- src/components/LeadForm.js
+++ src/components/LeadForm.js
@@ -2,7 +2,7 @@
 
 import { useState, useEffect, useCallback } from "react";
 import { useRouter } from "next/navigation";
-import FormInput from "./FormInput";
+import FormInput from "./table/FormInput";
 import { validateForm } from "../utils/validationUtil";
 
 const INITIAL_FORM_DATA = {
